534f824bd893246228b20ea6ad751208d692f213
abir <iamabir444@gmail.com>
2026-03-23 20:24:30 +0600

    handler added

--- main.go
+++ main.go
@@ -5,9 +5,19 @@ import (
 	"fmt"
 )
 
+func healthzHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type","text/plain; charset=utf-8")
+
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
+}
+
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/",http.FileServer(http.Dir(".")))
+	mux.Handle("/app/",http.StripPrefix("/app",http.FileServer(http.Dir("."))))
+	
+	mux.HandleFunc("/healthz", healthzHandler)
+
 	server := http.Server{
 		Addr: ":8080",
 		Handler: mux,
